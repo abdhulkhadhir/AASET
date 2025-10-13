2411e8299cac030fd3993301d0f63e023c34abcb
abdhulkhadhir <69329813+abdhulkhadhir@users.noreply.github.com>
2025-10-13 14:11:46 +1000

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def df_equivalent(a: pd.DataFrame, b: pd.DataFrame) -> bool:
         return False
 
 # --- LOGIN UI using streamlit-authenticator ---
-name, authentication_status, username = authenticator.login(location="main", fields={"Form name": "Login"})
+name, authentication_status, username = authenticator.login(location="main", max_login_attempts=3)
 
 if authentication_status is False:
     st.error("Username/password is incorrect")
